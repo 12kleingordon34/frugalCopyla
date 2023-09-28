ad0d5ad3597e07a69fe0f63297fb5d439ab22515
Dan Manela <danielmanela@gmail.com>
2023-09-28 16:29:54 +0100

    Finish demo

--- examples/demos/DeMO.py
+++ examples/demos/DeMO.py
@@ -3,12 +3,13 @@ import jax.numpy as jnp
 import numpyro
 numpyro.set_host_device_count(4)
 import numpyro.distributions as dist
+import numpy as np
 import pandas as pd
 from scipy.special import expit
 
 from frugalCopyla import copula_lpdfs
 from frugalCopyla.model	import CopulaModel
-
+from frugalCopyla.diagnostics import *
 
 def generate_copyla_model():
     return {
@@ -36,18 +37,25 @@ def generate_copyla_model():
 
 
 def main():
+    num_samples = 20000
     model_template = generate_copyla_model()
     mod = CopulaModel(model_template)
     sim_data = mod.simulate_data(
-        num_warmup=10000,
-        num_samples=20000,
+        num_warmup=1000,
+        num_samples=num_samples,
         joint_status='continuous',
         num_chains=4,
         seed=0
     )
+    
+    # Check sampler diagnostics
+    assert check_rhat(sim_data['model'])
+    min_ess = check_ess(sim_data['model'])
+    subselect_samples_idx = int(np.ceil((4*num_samples)/min_ess))
+    
     pd.DataFrame(
         sim_data['data']
-    )[['X', 'Y', 'Z1', 'Z2']].to_csv('python_samples.csv')
+    )[['X', 'Y', 'Z1', 'Z2']][::subselect_samples_idx].to_csv('python_samples.csv')
     return None
 
 if __name__ == '__main__':
