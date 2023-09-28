f027dd44eb89f4ea317ac23451a5f0a2b5f50178
Dan Manela <danielmanela@gmail.com>
2023-09-28 15:23:23 +0100

    Fix model bugs

--- setup.py
+++ setup.py
@@ -7,25 +7,28 @@ setup(
     author='Dan Manela',
     author_email='tbc',
     install_requires=[
-        'jax==0.4.7',
-        'jaxlib==0.4.6',
-        'jupyter-server==1.23.0',
-        'jupyter_client==7.4.4',
-        'jupyter_core==4.11.2',
-        'jupyterlab==3.5.0',
-        'jupyterlab-pygments==0.2.2',
-        'jupyterlab_server==2.16.2',
+        #'jax==0.4.16',
+        #'jaxlib==0.4.14',
+        #'jupyter-server==1.23.0',
+        #'jupyter_client==7.4.4',
+        #'jupyter_core==4.11.2',
+        #'jupyterlab==3.5.0',
+        #'jupyterlab-pygments==0.2.2',
+        #'jupyterlab_server==2.16.2',
         'matplotlib==3.6.2',
         'matplotlib-inline==0.1.6',
-        'numpy==1.23.4',
-        'numpyro==0.11.0',
-        'pandas==1.5.3',
-	'patsy==0.5.3',
+        'numpy==1.24.1',
+        #'numpyro==0.13.2',
+        #'pandas==1.5.3',
+	    'patsy==0.5.3',
         'optax==0.1.4',
         'tensorflow_probability == 0.18.0'
     ],
+    #dependency_links=['git+git://github.com/12kleingordon34/numpyro.git@master#egg=numpyro'],
     packages=find_packages(
         where='frugalCopyla',
-        include=['model', 'copula_lpdfs']
+        include=[
+            'model', 'copula_lpdfs'
+        ]
     )
 )
