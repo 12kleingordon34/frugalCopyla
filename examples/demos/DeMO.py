4648cd10d1bd67756bb2a1eb99cd24c3b99b98ed
Dan Manela <danielmanela@gmail.com>
2023-10-02 15:43:39 +0100

    Add discrete example

--- examples/demos/DeMO.py
+++ examples/demos/DeMO.py
@@ -11,24 +11,26 @@ from frugalCopyla import copula_lpdfs
 from frugalCopyla.model	import CopulaModel
 from frugalCopyla.diagnostics import *
 
-def generate_static_model():
+
+def generate_dynamic_continuous_model():
     return {
-        'Z1': {'dist': dist.Normal, 'formula': {'loc': 'Z1 ~ 1', 'scale': 'Z1 ~ 1'}, 'coeffs': {'loc': [0.], 'scale': [1.]}, 'link': {}},
-        'Z2': {'dist': dist.BernoulliProbs, 'formula': {'probs': 'Z2 ~ 1'}, 'coeffs': {'probs': [0.5]}, 'link': {}},
-        'X': {'dist': dist.BernoulliProbs, 'formula': {'probs': 'X ~ Z1 + Z2'}, 'coeffs': {'probs': [0., 1., 1.]}, 'link': {'probs': jax.scipy.special.expit}},
-        'Y': {'dist': dist.BernoulliProbs, 'formula': {'probs': 'Y ~ X'}, 'coeffs': {'probs': [0., 1.]}, 'link': {'probs': jax.scipy.special.expit}},
+        'A': {'dist': dist.BernoulliProbs, 'formula': {'probs': 'A ~ 1'}, 'coeffs': {'probs': [0.5]}, 'link': {}},
+        'U': {'dist': dist.Uniform, 'formula': {'low': 'U ~ 1', 'high': 'U ~ 1'}, 'coeffs': {'low': [0.], 'high': [1.]}, 'link': {}},
+        'L': {'dist': dist.Exponential, 'formula': {'rate': 'L ~ A'}, 'coeffs': {'rate': [-0.3, 0.2]}, 'link': {'rate': jnp.exp}},
+        'B': {'dist': dist.BernoulliProbs, 'formula': {'probs': 'B ~ L + A'}, 'coeffs': {'probs': [-0.3, 0.4, 0.3]}, 'link': {'probs': jax.scipy.special.expit}},
+        'Y': {'dist': dist.Exponential, 'formula': {'rate': 'Y ~ A + B'}, 'coeffs': {'rate': [0.5, -0.2, -0.3]}, 'link': {'probs': jnp.exp}},
         'copula': {
             'class': copula_lpdfs.multivar_gaussian_copula_lpdf,
-            'vars': ['Z1', 'Z2', 'Y'],
+            'vars': ['L', 'U', 'Y'],
             'formula': {
-                'rho_Z1Y': 'cop ~ 1',
-                'rho_Z2Y': 'cop ~ 1',
-                'rho_Z1Z2': 'cop ~ 1'
+                'rho_LU': 'cop ~ 1',
+                'rho_LY': 'cop ~ 1',
+                'rho_UY': 'cop ~ 1'
             },
             'coeffs': {
-                'rho_Z1Z2': [2*expit(0.1) - 1],            
-                'rho_Z1Y': [2*expit(0.2) - 1],
-                'rho_Z2Y': [2*expit(0.3) - 1],
+                'rho_LU': [2*expit(1) - 1],            
+                'rho_LY': [2*expit(0.5) - 1],
+                'rho_UY': [2*expit(1) - 1],
             },
             'link': {}
             # 'misc': {}
@@ -36,13 +38,13 @@ def generate_static_model():
     }
 
 
-def generate_dynamic_model():
+def generate_dynamic_discrete_model():
     return {
         'A': {'dist': dist.BernoulliProbs, 'formula': {'probs': 'A ~ 1'}, 'coeffs': {'probs': [0.5]}, 'link': {}},
         'U': {'dist': dist.Uniform, 'formula': {'low': 'U ~ 1', 'high': 'U ~ 1'}, 'coeffs': {'low': [0.], 'high': [1.]}, 'link': {}},
         'L': {'dist': dist.Exponential, 'formula': {'rate': 'L ~ A'}, 'coeffs': {'rate': [-0.3, 0.2]}, 'link': {'rate': jnp.exp}},
         'B': {'dist': dist.BernoulliProbs, 'formula': {'probs': 'B ~ L + A'}, 'coeffs': {'probs': [-0.3, 0.4, 0.3]}, 'link': {'probs': jax.scipy.special.expit}},
-        'Y': {'dist': dist.Exponential, 'formula': {'rate': 'Y ~ A + B'}, 'coeffs': {'rate': [0.5, -0.2, -0.3]}, 'link': {'probs': jnp.exp}},
+        'Y': {'dist': dist.BernoulliProbs, 'formula': {'probs': 'Y ~ A + B'}, 'coeffs': {'probs': [0.5, -0.2, -0.3]}, 'link': {'probs': jax.scipy.special.expit}},
         'copula': {
             'class': copula_lpdfs.multivar_gaussian_copula_lpdf,
             'vars': ['L', 'U', 'Y'],
@@ -84,13 +86,13 @@ def run_samples(num_samples, model):
 
 def main():
     num_samples = 4000
-    static_model = generate_static_model()
-    dynamic_model = generate_dynamic_model()
-    static_samples = run_samples(num_samples, static_model)
-    dynamic_samples = run_samples(num_samples, dynamic_model)
+    continuous_model = generate_dynamic_continuous_model()
+    discrete_model = generate_dynamic_discrete_model()
+    continuous_samples = run_samples(num_samples, continuous_model)
+    discrete_samples = run_samples(num_samples, discrete_model)
 
-    static_samples[['Z1', 'Z2', 'X', 'Y']].to_csv('static_samples.csv', index=False)
-    dynamic_samples[['A', 'L', 'B', 'Y']].to_csv('dynamic_samples.csv', index=False)
+    continuous_samples[['A', 'L', 'B', 'Y']].to_csv('continuous_samples.csv', index=False)
+    discrete_samples[['A', 'L', 'B', 'Y']].to_csv('discrete_samples.csv', index=False)
     return None
 
 if __name__ == '__main__':
