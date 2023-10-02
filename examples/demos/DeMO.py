92a40744e37d19e9707ebe192ae86caee52c1ea2
Dan Manela <danielmanela@gmail.com>
2023-10-02 15:55:11 +0100

    Fix inf bug

--- examples/demos/DeMO.py
+++ examples/demos/DeMO.py
@@ -18,7 +18,7 @@ def generate_dynamic_continuous_model():
         'U': {'dist': dist.Uniform, 'formula': {'low': 'U ~ 1', 'high': 'U ~ 1'}, 'coeffs': {'low': [0.], 'high': [1.]}, 'link': {}},
         'L': {'dist': dist.Exponential, 'formula': {'rate': 'L ~ A'}, 'coeffs': {'rate': [-0.3, 0.2]}, 'link': {'rate': jnp.exp}},
         'B': {'dist': dist.BernoulliProbs, 'formula': {'probs': 'B ~ L + A'}, 'coeffs': {'probs': [-0.3, 0.4, 0.3]}, 'link': {'probs': jax.scipy.special.expit}},
-        'Y': {'dist': dist.Exponential, 'formula': {'rate': 'Y ~ A + B'}, 'coeffs': {'rate': [0.5, -0.2, -0.3]}, 'link': {'probs': jnp.exp}},
+        'Y': {'dist': dist.Exponential, 'formula': {'rate': 'Y ~ A + B'}, 'coeffs': {'rate': [0.5, -0.2, -0.3]}, 'link': {'rate': jnp.exp}},
         'copula': {
             'class': copula_lpdfs.multivar_gaussian_copula_lpdf,
             'vars': ['L', 'U', 'Y'],
