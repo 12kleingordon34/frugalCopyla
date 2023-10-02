f56ffb32fca99480ec876f02b74960b8325c3e49
Dan Manela <danielmanela@gmail.com>
2023-10-02 14:41:42 +0100

    Add Didilez and Havercroft example

--- examples/demos/DeMO.py
+++ examples/demos/DeMO.py
@@ -11,7 +11,7 @@ from frugalCopyla import copula_lpdfs
 from frugalCopyla.model	import CopulaModel
 from frugalCopyla.diagnostics import *
 
-def generate_copyla_model():
+def generate_static_model():
     return {
         'Z1': {'dist': dist.Normal, 'formula': {'loc': 'Z1 ~ 1', 'scale': 'Z1 ~ 1'}, 'coeffs': {'loc': [0.], 'scale': [1.]}, 'link': {}},
         'Z2': {'dist': dist.BernoulliProbs, 'formula': {'probs': 'Z2 ~ 1'}, 'coeffs': {'probs': [0.5]}, 'link': {}},
@@ -36,10 +36,34 @@ def generate_copyla_model():
     }
 
 
-def main():
-    num_samples = 20000
-    model_template = generate_copyla_model()
-    mod = CopulaModel(model_template)
+def generate_dynamic_model():
+    return {
+        'A': {'dist': dist.BernoulliProbs, 'formula': {'probs': 'A ~ 1'}, 'coeffs': {'probs': [0.5]}, 'link': {}},
+        'U': {'dist': dist.Uniform, 'formula': {'low': 'U ~ 1', 'high': 'U ~ 1'}, 'coeffs': {'low': [0.], 'high': [1.]}, 'link': {}},
+        'L': {'dist': dist.Exponential, 'formula': {'rate': 'L ~ A'}, 'coeffs': {'rate': [-0.3, 0.2]}, 'link': {'rate': jnp.exp}},
+        'B': {'dist': dist.BernoulliProbs, 'formula': {'probs': 'B ~ L + A'}, 'coeffs': {'probs': [-0.3, 0.4, 0.3]}, 'link': {'probs': jax.scipy.special.expit}},
+        'Y': {'dist': dist.Exponential, 'formula': {'rate': 'Y ~ A + B'}, 'coeffs': {'rate': [0.5, -0.2, -0.3]}, 'link': {'probs': jnp.exp}},
+        'copula': {
+            'class': copula_lpdfs.multivar_gaussian_copula_lpdf,
+            'vars': ['L', 'U', 'Y'],
+            'formula': {
+                'rho_LU': 'cop ~ 1',
+                'rho_LY': 'cop ~ 1',
+                'rho_UY': 'cop ~ 1'
+            },
+            'coeffs': {
+                'rho_LU': [2*expit(1) - 1],            
+                'rho_LY': [2*expit(0.5) - 1],
+                'rho_UY': [2*expit(1) - 1],
+            },
+            'link': {}
+            # 'misc': {}
+        }
+    }
+
+
+def run_samples(num_samples, model):
+    mod = CopulaModel(model)
     sim_data = mod.simulate_data(
         num_warmup=1000,
         num_samples=num_samples,
@@ -53,9 +77,20 @@ def main():
     min_ess = check_ess(sim_data['model'])
     subselect_samples_idx = int(np.ceil((4*num_samples)/min_ess))
     
-    pd.DataFrame(
+    return pd.DataFrame(
         sim_data['data']
-    )[['X', 'Y', 'Z1', 'Z2']][::subselect_samples_idx].to_csv('python_samples.csv')
+    )[::subselect_samples_idx]
+
+
+def main():
+    num_samples = 4000
+    static_model = generate_static_model()
+    dynamic_model = generate_dynamic_model()
+    static_samples = run_samples(num_samples, static_model)
+    dynamic_samples = run_samples(num_samples, dynamic_model)
+
+    static_samples[['Z1', 'Z2', 'X', 'Y']].to_csv('static_samples.csv', index=False)
+    dynamic_samples[['A', 'L', 'B', 'Y']].to_csv('dynamic_samples.csv', index=False)
     return None
 
 if __name__ == '__main__':
